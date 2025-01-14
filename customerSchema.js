a7488f6f9c192bc954380877d4c9a1c8befe2678
Anton Prince Oommen <antonprince95@gmail.com>
2025-01-14 19:48:07 +0530

    "Inital commit"

--- customerSchema.js
+++ customerSchema.js
@@ -1,32 +1,5 @@
 import mongoose from "mongoose";
 
-const customersSchema = mongoose.Schema({
-    username: {
-        type:String,
-        required:true
-    },
-    name:{
-        type:String,
-        required:true
-    },
-    address:{
-        type:String,
-        required:true   
-    },
-    birthdate:{
-        type:Date,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    accounts:{
-        type:[Number],
-        requried:true
-    },
-    
 
-})
-
-export const customer = mongoose.model("customer", customersSchema)
+const customerSchema = new mongoose.Schema({}, {strict:false})
+export const customer = mongoose.model("customer", customerSchema)
