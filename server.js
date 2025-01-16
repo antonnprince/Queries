bdaa8aa741cb40f449a544b27333ef4e4c5e0f75
Anton Prince Oommen <antonprince95@gmail.com>
2025-01-16 20:15:48 +0530

    working here

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose.connect(process.env.mongoURL).then(()=>{
     app.listen(3000,()=>console.log("Server running at 3000"))
 })
 
+// fetch all users
 app.get('/all', async(req, res)=>{
     try {
         const result = await customer.find({})
@@ -22,18 +23,7 @@ app.get('/all', async(req, res)=>{
     }
 })
 
-app.get('/find_one', async(req,res)=>{
-    try {
-        const result = await customer.findOne({
-            "username":"hymen69"
-        })
-        return res.status(200).json(result)
-    } catch (error) {
-        console.log(error)
-        return res.status(404).json({message:"Error in fetching data"})
-    }
-})
-
+//find one with field specified in body
 app.get('/find_one2', async(req,res)=>{
     try {
         const {username} = req.body
@@ -47,19 +37,18 @@ app.get('/find_one2', async(req,res)=>{
     }
 })
 
+//find one with field specified in parameter
 app.get('/find_one3/:username', async(req, res)=>{
     const {username} = req.params
     const result = await customer.findOne({username:username})
     return res.status(200).json(result)
 })
 
+// update user with value in parameter
 app.get('/find_one_update/:username', async (req, res) => {
     try {
         const { username } = req.params;
-
-        // New data for updating
-        const newData = { username: 'hymen69' }; // You might want to update other fields or take dynamic input
-
+        
         const result = await customer.findOneAndUpdate(
             { username: username }, // Find the document by username
             { $set: {username:"HYMEN69"} },      // Update operation
@@ -73,6 +62,7 @@ app.get('/find_one_update/:username', async (req, res) => {
     }
 });
 
+//create new document
  app.post('/create', async(req,res)=>{
     try {
         const data = await customer.create({username:"test"})
@@ -83,6 +73,7 @@ app.get('/find_one_update/:username', async (req, res) => {
     }
  })
 
+ //delete document
  app.delete('/delete', async(req,res)=>{
     try {
         const data = await customer.findOneAndDelete({usermname:"test"})
@@ -91,3 +82,54 @@ app.get('/find_one_update/:username', async (req, res) => {
         return res.status(400).json({message:"Error"})
     }
  })
+
+
+// update username of a document
+ app.put('/update_many', async(req,res)=>{
+    try {
+        const data = await customer.updateMany({username:"test"},
+             {$set: {username:"test2"}})
+        return res.status(200).json({message:"successful"})
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({message:"Error"})
+    }
+ })
+
+//  insert a new field using udpateMany method
+ app.post('/insert', async(req,res)=>{
+    try {
+        const d = await customer.updateMany({},{$set:{age:200}})
+        return res.status(200).json({message:"DONE"})
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({message:"Error"})
+    }
+ })
+
+//  increment age field
+app.put('/update_age', async(req,res)=>{
+    try {
+        const d = await customer.updateMany({}, {$inc:{age:100}})
+        return res.status(200).json({message:"Completed"})
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({message:"Error"})
+    }
+})
+
+
+app.get('/operators', async(req,res)=>{
+    // const d = await customer.find({age:{$gte:300}})
+    // const d= await customer.find({age: {$eq:300}})
+    // const d = await customer.find({age: {$gt:100}})
+    const d=await customer.find({age: {$lt:300}})
+    return res.status(200).json(d)
+})
+
+app.post('/test', async(req,res)=>{
+    const d = await customer.findOneAndUpdate({username:null}, {
+        $set:{username:"HELOO"}
+    })
+    return res.status(200).json(d)
+})
