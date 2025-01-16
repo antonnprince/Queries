2ff7f555e6a7f9cf4ef98a90b1d0a0afecdba21a
Anton Prince Oommen <antonprince95@gmail.com>
2025-01-16 19:28:47 +0530

    some more queries

--- server.js
+++ server.js
@@ -11,3 +11,65 @@ mongoose.connect(process.env.mongoURL).then(()=>{
     console.log("DB connected")
     app.listen(3000,()=>console.log("Server running at 3000"))
 })
+
+app.get('/all', async(req, res)=>{
+    try {
+        const result = await customer.find({})
+        return res.status(200).json(result)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({message:"Error in fetching data"})
+    }
+})
+
+app.get('/find_one', async(req,res)=>{
+    try {
+        const result = await customer.findOne({
+            "username":"hymen69"
+        })
+        return res.status(200).json(result)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({message:"Error in fetching data"})
+    }
+})
+
+app.get('/find_one2', async(req,res)=>{
+    try {
+        const {username} = req.body
+        const result = await customer.findOne({
+            "username":username
+        })
+        return res.status(200).json(result)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({message:"Error in fetching data"})
+    }
+})
+
+app.get('/find_one3/:username', async(req, res)=>{
+    const {username} = req.params
+    const result = await customer.findOne({username:username})
+    return res.status(200).json(result)
+})
+
+app.get('/find_one_update/:username', async (req, res) => {
+    try {
+        const { username } = req.params;
+
+        // New data for updating
+        const newData = { username: 'hymen69' }; // You might want to update other fields or take dynamic input
+
+        const result = await customer.findOneAndUpdate(
+            { username: username }, // Find the document by username
+            { $set: {username:"HYMEN69"} },      // Update operation
+            { new: true }           // Return the updated document
+        );
+
+        return res.status(200).json(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Error in updating data" });
+    }
+});
+
